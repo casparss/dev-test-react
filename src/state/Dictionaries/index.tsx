7c69806d6772e42901e5f4591ab10e90fb797081
Caspar Mac Book <casparss@users.noreply.github.com>
2018-10-29 14:44:07 +0000

    add mapping tests in the green

--- src/state/Dictionaries/index.tsx
+++ src/state/Dictionaries/index.tsx
@@ -4,6 +4,7 @@ import {
   DictionaryReducersInt,
   DictionaryInt
 } from './Dictionaries.types'
+import { arrayReplaceItem } from '../../utils/Transform'
 
 /**
  * Actions
@@ -62,9 +63,44 @@ const reducers: DictionaryReducersInt = {
   },
   removeDictionary: (state, action) =>
     state.filter(dictionary => dictionary.id !== action.payload.id ),
-  addMapping: () => {},
-  removeMapping: () => {}
+  addMapping: (state, action) => {
+    const transform = (mappings: any, mappingId: string) =>
+      [...mappings, mappingId]
+    return mappingHelper(state, action, transform)
+  },
+  removeMapping: (state, action) => {
+    const transform = (mappings: any, mappingId: string) => {
+      return mappings.filter((idToRemove: string) => idToRemove !== mappingId)
+    }
+    return mappingHelper(state, action, transform)
+  }
 }
+
+/**
+ * Helpers
+ */
+
+const mappingHelper = (state: any, action: any, transformFunc: any) => {
+  const { dictionaryId, mappingId } = action.payload
+  const dictionary: any = state.find(({ id }: { id: string }) => id === dictionaryId)
+
+  const mappings = transformFunc([...dictionary.mappings], mappingId)
+
+  const newDictionary = {
+    ...dictionary,
+    mappings
+  }
+
+  return arrayReplaceItem(
+    state,
+    ({ id }: { id: string }) => id === dictionaryId,
+    newDictionary
+  )
+}
+
+/**
+ * Reducers
+ */
 
 export const dictionaries = (state: DictionaryInt[] = [], action: any) => {
   switch(action.type) {
